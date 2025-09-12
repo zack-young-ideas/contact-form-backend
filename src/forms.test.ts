ee303040dbf12775a7f0863eb0afa4a07a1e076d
Zack Young <zyoung612@gmail.com>
2025-09-12 15:59:53 -0400

    Install XSS
    The XSS package was installed and the ContactForm class now uses
    this package to sanitize the values submitted by users.

--- src/forms.test.ts
+++ src/forms.test.ts
@@ -1,14 +1,19 @@
 import validator from 'validator';
+import xss from 'xss';
 import ContactForm from './forms.ts';
 
 jest.mock('validator');
+jest.mock('xss', () => jest.fn());
 
 beforeEach(() => {
+  jest.clearAllMocks();
+
   validator.isAlpha = jest.fn((input) => true);
   validator.isEmail = jest.fn((input) => true);
   validator.isMobilePhone = jest.fn((input) => true);
+
+  xss = xss.mockImplementation((input) => input);
 });
-afterEach(() => jest.resetAllMocks());
 
 describe('ContactForm', () => {
   describe('isValid method', () => {
@@ -91,6 +96,60 @@ describe('ContactForm', () => {
       expect(form.errors.length).toBe(2);
       expect(form.errors[0]).toBe('Invalid email address');
       expect(form.errors[1]).toBe('Invalid phone number');
+    });
+  });
+
+  describe('cleanedData', () => {
+    it('contains sanitized data', () => {
+      let form = new ContactForm({
+        firstName: 'John',
+        lastName: 'Smith',
+        email: 'jsmith@example.com',
+        phone: '+12345678901',
+        message: 'Hello',
+      });
+      form.isValid();
+
+      expect(form.cleanedData).toMatchObject({
+        firstName: 'John',
+        lastName: 'Smith',
+        email: 'jsmith@example.com',
+        phone: '+12345678901',
+        message: 'Hello',
+      });
+    });
+
+    it('throws error if isValid() method is not called first', () => {
+      let form = new ContactForm({
+        firstName: 'John',
+        lastName: 'Smith',
+        email: 'jsmith@example.com',
+        phone: '+12345678901',
+        message: 'Hello',
+      });
+
+      expect(() => form.cleanedData).toThrow('Data unavailable');
+    });
+
+    it('calls xss on each input', () => {
+      expect(xss).toHaveBeenCalledTimes(0);
+
+      let form = new ContactForm({
+        firstName: 'John',
+        lastName: 'Smith',
+        email: 'jsmith@example.com',
+        phone: '+12345678901',
+        message: 'Hello',
+      });
+      form.isValid();
+      form.cleanedData;
+
+      expect(xss).toHaveBeenCalledTimes(5);
+      expect(xss.mock.calls[0][0]).toMatch('John');
+      expect(xss.mock.calls[1][0]).toMatch('Smith');
+      expect(xss.mock.calls[2][0]).toMatch('jsmith@example.com');
+      expect(xss.mock.calls[3][0]).toMatch('+12345678901');
+      expect(xss.mock.calls[4][0]).toMatch('Hello');
     });
   });
 });
