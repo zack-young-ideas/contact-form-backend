f599d653a533db396833e50426d9a4aa25db5336
Zack Young <zyoung612@gmail.com>
2025-09-19 20:17:30 -0400

    Move database connection logic to database.ts file
    The bin/build.ts file previously performed some error checking to
    ensure that the environment variables needed to connect to the
    application's database were provided. Furthermore, the same file
    would create the object passed to the mysql2.createConnection
    function to connect to the database.
    
    This logic has all been moved to the src/database.ts file. It
    makes more sense for these functions to be defined in the file
    that contains all the functions needed to communicate with the
    database.

--- bin/build.ts
+++ bin/build.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
-import { ConnectionOptions } from 'mysql2';
 import database from '../src/database';
 
 // Retrieve the .env file.
@@ -22,38 +21,5 @@ if (argument !== undefined) {
 }
 dotenv.config({ path: envPath });
 
-// Validate database connection info from .env file.
-const requiredConnectionParams: {[index: string]:string | undefined} = {
-  DATABASE_USER: process.env.DATABASE_USER,
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-  DATABASE_NAME: process.env.DATABASE_NAME,
-  DATABASE_HOST: process.env.DATABASE_HOST,
-};
-for (const key in requiredConnectionParams) {
-  if (requiredConnectionParams[key] === undefined) {
-    throw Error(
-      `.env file is missing '${key}' value; this value is required`
-    );
-  }
-};
-
 // Perform database migrations.
-const connectionObject: ConnectionOptions = {
-  user: String(process.env.DATABASE_USER),
-  password: String(process.env.DATABASE_PASSWORD),
-  database: String(process.env.DATABASE_NAME),
-  host: String(process.env.DATABASE_HOST),
-};
-if (process.env.DATABASE_CHARSET !== undefined) {
-  connectionObject.charset = process.env.DATABASE_CHARSET;
-}
-if (process.env.DATABASE_PORT !== undefined) {
-  connectionObject.port = Number(process.env.DATABASE_PORT);
-}
-if (process.env.DATABASE_LOCALADDRESS !== undefined) {
-  connectionObject.localAddress = process.env.DATABASE_LOCALADDRESS;
-}
-if (process.env.DATABASE_TIMEZONE !== undefined) {
-  connectionObject.timezone = process.env.DATABASE_TIMEZONE;
-}
-database.migrate(connectionObject);
+database.migrate();
--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,49 @@
 import mysql, { ConnectionOptions } from 'mysql2';
 import ContactForm from './forms';
 
-const getConnection = async (connectionOptions: ConnectionOptions) => {
-  const connection = await mysql.createConnection(connectionOptions);
+const validateConnParams = () => {
+  /*
+  Ensures that the required connection parameters are available as
+  environment variables.
+  */
+  const requiredConnectionParams: {[index: string]:string | undefined} = {
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    DATABASE_HOST: process.env.DATABASE_HOST,
+  };
+  for (const key in requiredConnectionParams) {
+    if (requiredConnectionParams[key] === undefined) {
+      throw Error(
+        `Environment variable '${key}' is undefined`
+      );
+    }
+  };
+}
+
+const getConnection = async () => {
+  /*
+  Connects to the MySQL database.
+  */
+  const connectionObject: ConnectionOptions = {
+    user: String(process.env.DATABASE_USER),
+    password: String(process.env.DATABASE_PASSWORD),
+    database: String(process.env.DATABASE_NAME),
+    host: String(process.env.DATABASE_HOST),
+  };
+  if (process.env.DATABASE_CHARSET !== undefined) {
+    connectionObject.charset = process.env.DATABASE_CHARSET;
+  }
+  if (process.env.DATABASE_PORT !== undefined) {
+    connectionObject.port = Number(process.env.DATABASE_PORT);
+  }
+  if (process.env.DATABASE_LOCALADDRESS !== undefined) {
+    connectionObject.localAddress = process.env.DATABASE_LOCALADDRESS;
+  }
+  if (process.env.DATABASE_TIMEZONE !== undefined) {
+    connectionObject.timezone = process.env.DATABASE_TIMEZONE;
+  }
+  const connection = await mysql.createConnection(connectionObject);
   return connection;
 }
 
@@ -12,9 +53,12 @@ const database = {
     return;
   },
 
-  migrate: async (connectionOptions: ConnectionOptions) => {
+  migrate: async () => {
+    /*
+    Creates the table needed to store each user's contact info.
+    */
     try {
-      const conn = await getConnection(connectionOptions);
+      const conn = await getConnection();
       await conn.execute(
         `CREATE TABLE IF NOT EXISTS contact_form_submissions (
 id INT AUTO_INCREMENT PRIMARY KEY,
@@ -36,4 +80,5 @@ created_at DATETIME DEFAULT CURRENT_TIMESTAMP
   }
 };
 
+export { getConnection, validateConnParams };
 export default database;
