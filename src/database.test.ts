f063978c1b797d2c6c8492f1af0b2813873f5067
zack-young-ideas <zackyoung8709@gmail.com>
2025-09-19 20:17:30 -0400

    Move database connection logic to database.ts file
    The bin/build.ts file previously performed some error checking to
    ensure that the environment variables needed to connect to the
    application's database were provided. Furthermore, the same file
    would create the object passed to the mysql2.createConnection
    function to connect to the database.
    
    This logic has all been moved to the src/database.ts file. It
    makes more sense for these functions to be defined in the file
    that contains all the functions needed to communicate with the
    database.

---
+++ src/database.test.ts
@@ -0,0 +1,60 @@
+import { getConnection, validateConnParams } from './database.ts';
+import mysql from 'mysql2';
+
+jest.mock('mysql2', () => ({
+  createConnection: jest.fn(),
+}));
+
+beforeEach(() => {
+  process.env.DATABASE_USER = 'user';
+  process.env.DATABASE_PASSWORD = 'supersecret';
+  process.env.DATABASE_NAME = 'database';
+  process.env.DATABASE_HOST = 'localhost';
+});
+
+describe('validateConnParams', () => {
+  it('should throw error if required parameters are missing', () => {
+    expect(() => validateConnParams()).not.toThrow();
+
+    delete process.env.DATABASE_USER;
+
+    expect(() => validateConnParams()).toThrow(
+      "Environment variable 'DATABASE_USER' is undefined"
+    );
+
+    process.env.DATABASE_USER = 'user';
+    delete process.env.DATABASE_HOST;
+
+    expect(() => validateConnParams()).toThrow(
+      "Environment variable 'DATABASE_HOST' is undefined"
+    );
+
+    process.env.DATABASE_HOST = 'localhost';
+    delete process.env.DATABASE_PASSWORD;
+
+    expect(() => validateConnParams()).toThrow(
+      "Environment variable 'DATABASE_PASSWORD' is undefined"
+    );
+  });
+});
+
+describe('getConnection', () => {
+  it('should call mysql.createConnection method', async () => {
+    process.env.DATABASE_CHARSET = 'utf8mb4';
+    process.env.DATABASE_PORT = '3306';
+
+    expect(mysql.createConnection).toHaveBeenCalledTimes(0);
+
+    await getConnection();
+
+    expect(mysql.createConnection).toHaveBeenCalledTimes(1);
+    expect(mysql.createConnection).toHaveBeenCalledWith({
+      user: 'user',
+      password: 'supersecret',
+      database: 'database',
+      host: 'localhost',
+      charset: 'utf8mb4',
+      port: 3306,
+    });
+  });
+});
