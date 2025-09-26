dcfd9b4ff8493c104a21afd26554dc45a64247d0
Zack Young <zyoung612@gmail.com>
2025-09-26 19:34:04 -0400

    Define the functions that send emails
    When a user submits a POST request containing their contact info,
    two functions are called: One sends an instant acknowledgement
    email to the user letting them know that their contact information
    was received, and the other sends an email to the admin letting
    them know that someone has requested to be contacted.
    
    These two functions now actually send email to both parties, as
    expected. Several tests were written to ensure these functions
    actually do what they're supposed to.

--- src/routes/routes.test.ts
+++ src/routes/routes.test.ts
@@ -1,6 +1,6 @@
 import { postHandler } from './routes';
 import database from '../database';
-import mail from '../mail';
+import { sendAcknowledgementEmail, sendAdminEmail } from '../mail';
 // eslint-disable-next-line
 import ContactForm from '../forms';
 
@@ -8,15 +8,34 @@ jest.mock('../database');
 jest.mock('../forms');
 jest.mock('../mail');
 
+// Mock values used by the tests below.
+let request;
+let response;
+let formObject;
+let postData;
+
+beforeEach(() => {
+  // Defines mock values used by request handler function.
+  postData = {
+    firstName: 'John',
+    lastName: 'Smith',
+    email: 'jsmith@example.com',
+    phone: '+12345678901',
+    message: 'Hello',
+  }
+  request = { body: postData };
+  response = { status: () => ({ json: () => null }) };
+  formObject = {
+    isValid: () => true,
+    cleanedData: postData,
+  };
+  ContactForm = jest.fn(() => formObject);
+});
+
 afterEach(() => jest.resetAllMocks());
 
 describe('postHandler', () => {
   it('should create database entry', async () => {
-    const request = {};
-    const response = { status: () => ({ json: () => null }) };
-    const formObject = { isValid: () => true };
-    ContactForm = jest.fn(() => formObject);
-
     expect(database.createContact).toHaveBeenCalledTimes(0);
     
     await postHandler(request, response);
@@ -26,47 +45,22 @@ describe('postHandler', () => {
   });
 
   it('should send confirmation email', async () => {
-    const request = {};
-    const response = { status: () => ({ json: () => null }) };
-    const formObject = { isValid: () => true };
-    ContactForm = jest.fn(() => formObject);
-
-    expect(mail.sendMail).toHaveBeenCalledTimes(0);
+    expect(sendAcknowledgementEmail).toHaveBeenCalledTimes(0);
     
     await postHandler(request, response);
 
-    expect(mail.sendMail).toHaveBeenCalledTimes(1);
-    expect(mail.sendMail).toHaveBeenCalledWith({
-      template: 'contactForm.html',
-      variables: {
-        title: 'Thank You!',
-        content: ('Thank you for reaching out to us. A member of '
-                  + 'our team will contact you shortly.'),
-      }
-    });
+    expect(sendAcknowledgementEmail).toHaveBeenCalledTimes(1);
+    expect(sendAcknowledgementEmail).toHaveBeenCalledWith(
+      'jsmith@example.com'
+    );
   });
 
   it('should send email to admin', async () => {
-    const request = {
-      body: {
-        firstName: 'John',
-        lastName: 'Smith',
-        email: 'jsmith@example.com',
-        phone: '+12345678901',
-        message: 'Hello',
-      }
-    };
-    const response = { status: () => ({ json: () => null }) };
-    const formObject = { isValid: () => true };
-    ContactForm = jest.fn(() => formObject);
-
-    expect(mail.contactAdmin).toHaveBeenCalledTimes(0);
+    expect(sendAdminEmail).toHaveBeenCalledTimes(0);
     
     await postHandler(request, response);
 
-    expect(mail.contactAdmin).toHaveBeenCalledTimes(1);
-    expect(mail.contactAdmin).toHaveBeenCalledWith({
-      content: ''
-    });
+    expect(sendAdminEmail).toHaveBeenCalledTimes(1);
+    expect(sendAdminEmail).toHaveBeenCalledWith(postData);
   });
 });
